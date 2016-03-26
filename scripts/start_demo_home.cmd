6ed167f5c1868407994a66c538e25eba860eecef
YuryPol <ayurip@gmail.com>
2016-03-26 15:01:12 -0700

    more data to process

--- scripts/start_demo_home.cmd
+++ scripts/start_demo_home.cmd
@@ -1,6 +1,5 @@
-cd C:\Users\Yury\Documents\GitHub\TestModel\scripts
 "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" "--defaults-file=C:\ProgramData\MySQL\MySQL Server 5.6\my.ini" "--verbose" "-uroot" "-pIraAnna12" < "C:\Users\Yury\Documents\GitHub\TestModel\sql\InitDemo.sql"
 
-java -cp "C:/Program Files (x86)/MySQL/MySQL Connector J/mysql-connector-java-5.1.36-bin.jar";C:/Users/Yury/Documents/GitHub/TestModel/target/TestModel-0.0.1-SNAPSHOT-jar-with-dependencies.jar ProcessInputInc
+java -cp "C:/Program Files (x86)/MySQL/MySQL Connector J/mysql-connector-java-5.1.36-bin.jar";C:/TestModel/TestModel/target/TestModel-0.0.1-SNAPSHOT-jar-with-dependencies.jar ProcessInputInc C:/TestModel/TestModel/Input/document_1.json
 
 "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" "--defaults-file=C:\ProgramData\MySQL\MySQL Server 5.6\my.ini" "--verbose" "-uroot" "-pIraAnna12" < "C:\Users\Yury\Documents\GitHub\TestModel\sql\RunDemo.sql"
