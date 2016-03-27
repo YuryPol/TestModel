9a171ef7eb6323fbac62ec509ecb168161d11f92
YuryPol <ayurip@gmail.com>
2016-03-27 11:14:10 -0700

    fast simulation
    Pass number of hits as a parameter, thus making it a sample data with
    count=  count/parameter

--- src/Simulation.java
+++ src/Simulation.java
@@ -18,6 +18,7 @@ public class Simulation {
         String url = "jdbc:mysql://localhost:3306/demo";
         String user = "root";
         String password = "IraAnna12";
+        int increment = Integer.parseInt(args[0].trim());
         int served_count = 0;
         int missed_count = 0;
         
@@ -42,16 +43,12 @@ public class Simulation {
             PreparedStatement choseInventorySet = con.prepareStatement(
             		"select set_key_is, (goal-served_count)/goal as weight_now from result_serving "
             		+ "where goal > served_count and ( ? & set_key_is ) = set_key_is order by weight_now desc limit 1;");
-            PreparedStatement incrementServedCount = con.prepareStatement("update result_serving set served_count = served_count + 1 where set_key_is = ?");
-//            PreparedStatement changeRawServedCount = con.prepareStatement(
-//            		"update raw_inventory_used set "
-//            		+ " served_count = case when ? & ? != 0 then served_count + 1 else served_count end,"
-//            		+ " missed_count = case when ? & ?  = 0 then missed_count + 1 else missed_count end"
-//            		+ " where basesets = ?;");
+            PreparedStatement incrementServedCount = con.prepareStatement("update result_serving set served_count = served_count + "
+            		+ Integer.toString(increment) + " where set_key_is = ?");
             //
             // Process raw inventory
             //
-            for(int i = 0; i < max_weight; i++) {
+            for(int i = 0; i < max_weight; i += increment) {
 	            // create the request
 	            getRequest.setInt(1, rand.nextInt(max_weight));
 	            rs = getRequest.executeQuery();
@@ -69,22 +66,17 @@ public class Simulation {
 	            	set_key_is = rs.getLong(1);
 	            	incrementServedCount.setLong(1, set_key_is);
 	            	int result = incrementServedCount.executeUpdate();
-	            	served_count++;
+	            	served_count += increment;
 	            }
 	            else
 	            	missed_count++;
 	            // increment served_count in result_serving
-//	            changeRawServedCount.setLong(1, basesets);	// TODO: replace with setArray.            
-//	            changeRawServedCount.setLong(2, set_key_is);
-//	            changeRawServedCount.setLong(3, basesets);	            
-//	            changeRawServedCount.setLong(4, set_key_is);
-//	            changeRawServedCount.setLong(5, basesets);	            
-//	            changeRawServedCount.executeUpdate();
 	            if (served_count != 0 && served_count % 10000 == 0)
 	            {
 	                System.out.print(
 	                        new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date()));
-	                System.out.println(" : served_count=" +  String.valueOf(served_count) + ", missed_count=" + String.valueOf(missed_count));            	
+	                System.out.print(" : served_count=" +  String.valueOf(served_count) + ", missed_count=" 
+	                        + String.valueOf(missed_count) + "\r");            	
 	            }
             }
             System.out.print(
